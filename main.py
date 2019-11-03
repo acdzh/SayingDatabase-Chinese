f624fb8a0c24351fc6d6367bd89f74f414bf418b
acdzh <32260125+acdzh@users.noreply.github.com>
2019-11-03 15:40:49 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+if __name__ == "__main__":
+    str = ""
+    ff = set(())
+    with open('data.txt', encoding='utf-8') as f:
+        for line in f.readlines()[0:4]:
+            line = line.strip()
+            a = line.find('$$')
+            b = line.find('$$', a + 2)
+            name = line[a + 2: b]
+            saying = line[b + 2 : ]
+            print (name, saying)
